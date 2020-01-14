52f1ae63aa5d79674b87ed31bd0a57895697f72b
Jerry Chen <raychen31638@gmail.com>
2020-01-14 14:48:15 -0600

    finished

---
+++ src/ducks/budgetReducer.js
@@ -0,0 +1,81 @@
+import axios from 'axios';
+
+let initialState = {
+    purchases: [],
+    budgetLimit: null,
+    loading: false
+}
+
+const REQUEST_BUDGET_DATA = 'REQUEST_BUDGET_DATA';
+const ADD_PURCHASE = 'ADD_PURCHASE';
+const REMOVE_PURCHASE = 'REMOVE_PURCHASE';
+
+function reducer(state = initialState, action){
+    switch(action.type){
+        case `${REQUEST_BUDGET_DATA}_PENDING`:
+            return {
+                ...state,
+                loading: true
+            };
+        case `${REQUEST_BUDGET_DATA}_FULFILLED`:
+            return {
+                ...state,
+                loading: false,
+                purchases: action.payload.purchases,
+                budgetLimit: action.payload.budgetLimit
+            };
+        case `${ADD_PURCHASE}_PENDING`:
+            return {
+                ...state,
+                loading: true
+            };
+        case `${ADD_PURCHASE}_FULFILLED`:
+            console.log(action.payload)
+            return {
+                ...state,
+                loading: false,
+                purchases: action.payload
+            };
+        case `${REMOVE_PURCHASE}_PENDING`:
+            return {
+                ...state,
+                loading: true
+            };
+        case `${REMOVE_PURCHASE}_FULFILLED`:
+                console.log(action.payload)
+            return {
+                ...state,
+                loading: false,
+                purchases: action.payload
+            };
+        default: return state;
+    }
+    
+}
+
+export function requestBudgetData(){
+    let data = axios.get('/api/budget-data').then(res=>res.data);
+    return {
+        type: REQUEST_BUDGET_DATA,
+        payload: data
+    }
+}
+
+export function addPurchase(price, description, category){
+    const obj = {price, description, category};
+    let data = axios.post('/api/budget-data/purchase', obj).then(res=>res.data);
+    return {
+        type: ADD_PURCHASE,
+        payload: data
+    }
+}
+
+export function removePurchase(id){
+    let data = axios.delete(`/api/budget-data/purchase/${id}`).then(res=>res.data);
+    return {
+        type: REMOVE_PURCHASE,
+        payload: data
+    }
+}
+
+export default reducer;
